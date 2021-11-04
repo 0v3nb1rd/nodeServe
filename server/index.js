b06bcb4e06bb50d7edc840166f59fc9c28f5a8f5
0v3nb1rd <0v3nb1rd@gmail.com>
2021-11-04 12:31:44 +0200

    add static folder && get response

--- server/index.js
+++ server/index.js
@@ -1,16 +1,30 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = process.env.PORT || 3010;
+const port = process.env.PORT || 8080;
 const fs = require('fs');
 
-app.get('/users', async (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../', 'index.html'));
-  let db = fs.readFileSync(path.resolve(__dirname, '../DB/users.json'));
-  let users = JSON.parse(db);
+app.use(express.static('./client')); // add static folder as middleware
 
-  res.header('Access-Control-Allow-Origin', '*');
-  res.send(users);
+app.get('/:name', async (req, res) => {
+  res.header('Content-Type', 'application/json'); // Add app.json header
+  res.header('Access-Control-Allow-Origin', '*'); // allow CORS
+
+  const { name } = req.params;
+  const usersFilePath = path.join(__dirname, name);
+
+  if (fs.existsSync(usersFilePath)) {
+    try {
+      const file = fs.readFileSync(usersFilePath, 'utf8');
+      res.send(file);
+    } catch (err) {
+      console.log(err.message);
+    }
+  } else {
+    res.status(404);
+    res.send('Sorry, file not found :-(');
+    console.log('404');
+  }
 });
 
 app.listen(port, () => {
